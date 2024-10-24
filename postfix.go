4583692dc93e51aaa6c3d30b00400114c6e65465
John-Bush14 <joppe@stuyck.be>
2024-10-24 20:20:33 +0200

    feat: precedence()

--- postfix.go
+++ postfix.go
@@ -16,4 +16,14 @@ type Operator struct {
    Operator OperatorEnum
 }
 
-
+func (o *Operator) precedence() int {
+   switch o.Operator {
+      case Plus: return 0
+      case Minus: return 0
+      case Asterix: return 1
+      case Slash: return 1
+      case Circumflex: return 2
+      case Radical: return 2
+      default: return 9999
+   }
+}
