6fc167a85d209e4d927252b562fa881455492750
John-Bush14 <joppe@stuyck.be>
2024-10-24 21:00:19 +0200

    feat: infix to postfix (without parantheses)

--- main.go
+++ main.go
@@ -1,4 +1,7 @@
 package main
 
+import "fmt" 
+
 func main() {
+   fmt.Print(infixToPostfix("a+b*c+d"))
 }
--- postfix.go
+++ postfix.go
@@ -1,5 +1,51 @@
 package main
 
+import "fmt"
+
+
+func infixToPostfix(infix string) string {
+   var postfix string
+
+   var lastPrescedence int = 0
+
+   var stack []Operator
+
+   for _, char := range infix {
+      fmt.Print(string(char), ": char ", postfix, ": postfix ", stack, ": stack \n")
+
+      if isOperator(char) {
+         var operator Operator = Operator{
+            Operator: OperatorEnum(char),
+         }
+
+         for operator.precedence() <= lastPrescedence && len(stack) > 0 {
+            var lastItem Operator = stack[len(stack)-1]
+
+            stack = stack[:len(stack)-1]
+
+            postfix += string(rune(lastItem.Operator))
+         }
+         
+         stack = append(stack, operator);
+         lastPrescedence = operator.precedence()
+
+         continue
+      }
+
+      if char == ' ' {continue}
+
+      postfix += string(rune(char));
+   }
+
+   for len(stack) > 0 {
+      postfix += string(rune(stack[len(stack)-1].Operator))
+
+      stack = stack[:len(stack)-1]
+   }
+
+   return postfix
+}
+
 
 type OperatorEnum rune
 
