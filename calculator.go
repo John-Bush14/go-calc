f47059651bbce60ee14b297d9235289d653d3cff
John-Bush14 <joppe@stuyck.be>
2024-10-24 21:51:32 +0200

    feat: calculate postfix expression

--- calculator.go
+++ calculator.go
@@ -1,1 +1,57 @@
 package main
+
+import (
+	"math"
+	"strconv"
+)
+
+
+func calculatePostfix(postfix string) float64 {
+   var stack []string
+
+   stack = append(stack, "")
+
+   for _, char := range postfix {
+      if isOperator(char) {
+         x, _ := strconv.ParseFloat(stack[len(stack)-2], 64)
+         y, _ := strconv.ParseFloat(stack[len(stack)-1], 64)
+
+         print(x, " ", y, "", string(char), "\n")
+
+         stack = append(stack[:len(stack)-2], strconv.FormatFloat(calculate(OperatorEnum(char), x, y), 'f', 2, 64))
+
+         continue
+      }
+
+      if char == ' ' {
+         stack = append(stack, "")
+
+         continue
+      }
+
+      stack[len(stack)-1] += string(char)
+   }
+
+   ans, _ := strconv.ParseFloat(stack[0], 64)
+
+   return ans
+}
+
+func calculate(operator OperatorEnum, x float64, y float64) float64 {
+   switch rune(operator) {
+      case '+': 
+         return x + y
+      case '-':
+         return x - y
+      case '*':
+         return x * y
+      case '/':
+         return x / y
+      case '^':
+         return math.Pow(x, y)
+      case '√':
+         if y == 2 {return math.Sqrt(x)}
+   }
+
+   return 0
+}
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "fmt" 
 
 func main() {
-   fmt.Print(infixToPostfix("A+(BC+D)/E+FG"))
+   fmt.Print(calculatePostfix(infixToPostfix("(3+5^2-2*16√2)/(4+6-2^3)*(9-3*25√2)+(7^2-3^3+2*49√2)")))
 }
