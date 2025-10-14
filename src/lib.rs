75a6f5e785556351899093e0c33fa46c760fe8e9
John-Bush14 <joppe@stuyck.be>
2025-10-14 13:35:44 +0200

    feat(go-flex): .calculate function for operators

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,23 @@ pub enum Token {
 pub enum Side {Left, Right}
 
 macro_rules! define_operators {
-    ($vis:vis $enum_name:ident {$( $name:ident $(,)? )*}) => {
+    ($a:ident, $b:ident, $vis:vis $enum_name:ident {$( $name:ident($operation:expr) $(,)? )*}) => {
         $vis enum $enum_name {
             $($name,)*
         }
+
+        impl $enum_name {
+            fn calculate(&self, $a: f64, $b: f64) -> f64 { match self {
+                $($enum_name::$name => ($operation),)*
+            }}
+        }
     };
 }
-
-define_operators!(pub Operator {
-    Addition,
-    Negation,
-    Multiplication,
-    Division,
-    Exponentiation,
-    RootExtraction
+define_operators!(x, y, pub Operator {
+    Addition(x+y),
+    Negation(x-y),
+    Multiplication(x*y),
+    Division(x/y),
+    Exponentiation(x.powf(y)),
+    RootExtraction(x.powf(1f64/y))
 });
