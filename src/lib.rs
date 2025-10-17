1168b72abffb0d6f25dd7689fdd4edbc272842dd
John-Bush14 <joppe@stuyck.be>
2025-10-17 14:33:40 +0200

    refactor: move macro to macros.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
+mod macros;
+
+use crate::define_operators;
 use derive_more::Display;
+
 
 #[derive(Display)]
 #[display("{_0}")]
@@ -16,29 +20,6 @@ pub enum Side {
     Right
 }
 
-
-macro_rules! define_operators {
-    ($a:ident, $b:ident, $vis:vis $enum_name:ident {$( $name:ident($identifier:expr) = $operation:expr ),*}) => {
-        #[derive(Display)]
-        $vis enum $enum_name {
-            $(
-                #[display($identifier)]
-                $name
-            ),*
-        }
-
-        impl $enum_name {
-            fn calculate(&self, $a: f64, $b: f64) -> f64 { match self {
-                $($enum_name::$name => ($operation),)*
-            }}
-
-            fn identify(operator_str: &str) -> Option<Operator> { match operator_str {
-                $( $identifier => Some(Operator::$name),)*
-                _ => None
-            }}
-        }
-    };
-}
 define_operators!(x, y, pub Operator {
     Addition("+") = x+y,
     Negation("-") = x-y,
@@ -47,3 +28,5 @@ define_operators!(x, y, pub Operator {
     Exponentiation("^") = x.powf(y),
     RootExtraction("√") = x.powf(1f64/y)
 });
+
+mod lexer;
---
+++ src/macros.rs
@@ -0,0 +1,23 @@
+#[macro_export]
+macro_rules! define_operators {
+    ($a:ident, $b:ident, $vis:vis $enum_name:ident {$( $name:ident($identifier:expr) = $operation:expr ),*}) => {
+        #[derive(Display)]
+        $vis enum $enum_name {
+            $(
+                #[display($identifier)]
+                $name
+            ),*
+        }
+
+        impl $enum_name {
+            fn calculate(&self, $a: f64, $b: f64) -> f64 { match self {
+                $($enum_name::$name => ($operation),)*
+            }}
+
+            fn identify(operator_str: &str) -> Option<Operator> { match operator_str {
+                $( $identifier => Some(Operator::$name),)*
+                _ => None
+            }}
+        }
+    };
+}
