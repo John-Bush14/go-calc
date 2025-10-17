697afe64043699bbd97a477574dd12219144c8a4
John-Bush14 <joppe@stuyck.be>
2025-10-17 14:31:37 +0200

    feat(go): everything derives Display (correctly)

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,49 @@
+use derive_more::Display;
+
+#[derive(Display)]
+#[display("{_0}")]
 pub enum Token {
     Constant(f64),
     Operator(Operator),
     Bracket(Side)
 }
 
-pub enum Side {Left, Right}
+#[derive(Display)]
+pub enum Side {
+    #[display("(")]
+    Left,
+    #[display(")")]
+    Right
+}
+
 
 macro_rules! define_operators {
-    ($a:ident, $b:ident, $vis:vis $enum_name:ident {$( $name:ident($operation:expr) $(,)? )*}) => {
+    ($a:ident, $b:ident, $vis:vis $enum_name:ident {$( $name:ident($identifier:expr) = $operation:expr ),*}) => {
+        #[derive(Display)]
         $vis enum $enum_name {
-            $($name,)*
+            $(
+                #[display($identifier)]
+                $name
+            ),*
         }
 
         impl $enum_name {
             fn calculate(&self, $a: f64, $b: f64) -> f64 { match self {
                 $($enum_name::$name => ($operation),)*
             }}
+
+            fn identify(operator_str: &str) -> Option<Operator> { match operator_str {
+                $( $identifier => Some(Operator::$name),)*
+                _ => None
+            }}
         }
     };
 }
 define_operators!(x, y, pub Operator {
-    Addition(x+y),
-    Negation(x-y),
-    Multiplication(x*y),
-    Division(x/y),
-    Exponentiation(x.powf(y)),
-    RootExtraction(x.powf(1f64/y))
+    Addition("+") = x+y,
+    Negation("-") = x-y,
+    Multiplication("*") = x*y,
+    Division("/") = x/y,
+    Exponentiation("^") = x.powf(y),
+    RootExtraction("√") = x.powf(1f64/y)
 });
